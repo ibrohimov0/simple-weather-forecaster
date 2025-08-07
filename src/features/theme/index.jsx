7aec8e9b1df53afcf53710bdcf28db94644774f4
Abdulloh <ibrohimovabdulloh573@gmail.com>
2025-08-07 22:29:27 +0500

    theme added

--- src/features/theme/index.jsx
+++ src/features/theme/index.jsx
@@ -1,8 +1,20 @@
+import { Moon, Sun } from "@phosphor-icons/react";
+import { useState } from "react";
+
 export default function ChangeTheme() {
-    
+    const [theme,setTheme] = useState();
+    const changeTheme = () => {
+        const currentTheme = theme === "dark" ? "light" : "dark"
+        document.documentElement.setAttribute("data-theme",currentTheme)
+        setTheme(currentTheme)
+    }
     return (
-        <button>
-            <Sun weight="bold" className="icon" />
+        <button onClick={changeTheme}>
+            {theme === "dark" ? 
+                <Sun weight="bold" className="icon" />
+            :
+                <Moon weight="bold" className="icon"/>
+            }
         </button>
     )
 }
